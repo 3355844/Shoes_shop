3fa4e73873b1fb0734d8836950449669a1add049
Andrei Ponomarenko <3355844@gmail.com>
2017-10-12 13:53:03 +0300

    Comment form

--- Sites/com.ua.dog-training/js/main.js
+++ Sites/com.ua.dog-training/js/main.js
@@ -1,5 +1,5 @@
 var regForm, logo_text, logo_img, img, menu_home, menu_about, menu_puppies, menu_news, menu_dogFood, menu_contacts,
-    content, leftBar, rightBar, commentForm, loginWindow, rightMenu, divMenu;
+    content, leftBar, rightBar, commentForm, loginWindow, rightMenu, divMenu, enter, regWin, login;
 
 window.onload = function () {
     document.title = "Кинолгический Центр";
@@ -11,7 +11,7 @@ window.onload = function () {
     img = document.createElement("img");
     img.src = "img/logo.png";
     logo_img.appendChild(img);
-    logo_text.innerHTML = "Кинологический центр Юрия Лучко";
+    logo_text.innerHTML = "Site";
 
     // menu
 
@@ -42,19 +42,22 @@ window.onload = function () {
 
     // left bar
 
-    rightMenu = document.getElementById("left_bar_menu");
+    // rightMenu = document.getElementById("left_bar_menu");
     // divMenu = rightMenu.getElementsByTagName('div');
     // divMenu.style.display = "none";
 
     leftBar = document.getElementById("leftBar");
-
 
     // right content
 
     rightBar = document.getElementById("rightBar");
 
     // Registration Window
-
+    regWin = document.getElementById("regWin");
+    enter = document.getElementById("enter");
+    if (login !== undefined) {
+        enter.innerHTML = login;
+    }
     regForm = document.getElementById("register_form");
     regForm.style.display = "none";
 
@@ -64,9 +67,20 @@ window.onload = function () {
     loginWindow.style.display = "none";
 
     // Hide left menu elements
+
     hideAllLeftMenu();
 
+
 };
+
+function register() {
+    login = document.getElementById("login").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    // alert(login + email + password);
+    regWin.style.display = "none";
+    enter.value = login;
+}
 
 function hideAllLeftMenu() {
     var allElements = document.getElementsByClassName("left_bar_menu");
@@ -78,19 +92,33 @@ function hideAllLeftMenu() {
     }
 }
 
+function hideLeftMenuWithOutThis(menu) {
+    var allElements = document.getElementsByClassName("left_bar_menu");
+    for (var j = 0; j < allElements.length; j++) {
+        var element = allElements[j];
+        var allMenu = element.getElementsByTagName("div");
+        console.log(element.id);
+        if (menu === element.id) {
+        } else {
+            for (var i = 0; i < allMenu.length; i++) {
+                if (menu === allMenu[i].id) {
+                } else {
+                    allMenu[i].style.display = "none";
+                }
+            }
+        }
+    }
+}
+
 function showMenu(menu) {
-
     var menuPart = document.getElementById(menu).getElementsByTagName("div");
-    hideAllLeftMenu();
-
+    hideLeftMenuWithOutThis(menu);
     for (var i = 0; i < menuPart.length; i++) {
         var divMenuPart = menuPart[i];
         if (divMenuPart.style.display === "none") {
             divMenuPart.style.display = "block";
         } else {
-            hideAllLeftMenu();
-            return;
-            // divMenuPart.style.display = "none";
+            divMenuPart.style.display = "none";
         }
     }
 }
@@ -111,6 +139,27 @@ function showLoginForm() {
     } else {
         loginWindow.style.display = "none";
     }
+}
+
+function addComment() {
+    var email = document.getElementById("email_message");
+    var text_message = document.getElementById("text_message");
+    if (text_message.value === "" || email.value === "") {
+        alert("Please enter your Message");
+        return;
+    }
+    var name = "Guest";
+    var comments = document.getElementById("comments");
+    var sender = document.createElement("h4");
+    var date = new Date();
+    sender.innerHTML = name + "-" + date.toLocaleTimeString() + " : " + date.toLocaleDateString();
+    var message = document.createElement("a");
+    message.text = " :" + text_message.value;
+    sender.appendChild(message);
+    comments.insertBefore(sender, comments.firstElementChild);
+    var message_from = document.getElementById("message_form");
+    message_from.reset();
+
 }
 
 function regWin() {
